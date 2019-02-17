ce0d2d107063e39bbc82837b7447f50cb34ae976
meow <rungnapa.te@ku.th>
2019-02-17 11:06:02 +0700

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule
